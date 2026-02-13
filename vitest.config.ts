63947f8e46b8b5cc762b3c213c7bc9c8b446dc0f
Sebastián López O <sebastianlopezosorno2005@gmail.com>
2026-02-12 23:56:56 -0500

    Configure server settings and base path in Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+     server: {
+        host: "::",
+        port: 8080,
+    },
     plugins: [react()],
     test: {
         environment: "jsdom",
@@ -13,4 +17,5 @@ export default defineConfig({
     resolve: {
         alias: {"@": path.resolve(__dirname, "./src")},
     },
+    base: "/valentineday/",
 });
